719f3f484e1cf65d077494c6dc153b92b4d91992
Claire <clarrechen@gmail.com>
2024-06-01 01:12:42 -0500

    rectangle

--- cses/rectangle_cutting.cpp
+++ cses/rectangle_cutting.cpp
@@ -7,16 +7,40 @@ using namespace std;
 
 /*
 dp[i][j] = minimum cuts to cut i x j rectangle
-dp[i][j] = min(dp[i][j-k])
+dp[i][j] = min(dp[i][j-k] + dp[i][k] + 1, dp[i-k][j] + dp[k][j] + 1) for k = 1 to i-1
+dp[i][j] = 0 if i = 1 and j = 1
+
 */
-int main(){
+int main()
+{
+    // freopen("../input.txt", "r", stdin);
+    // freopen("../output.txt", "w", stdout);
     int a, b;
     cin >> a >> b;
-    vector<vector<int>> dp(a+1, vector<int>(b+1, inf)); // minimum cuts to cut axb rectangle
-    dp[1][1] = 0;
-    for (int i = 1; i <= a; ++i){
-        for (int j = 1; j <=b ; ++j){
+    vector<vector<int>> dp(a + 1, vector<int>(b + 1, inf)); // minimum cuts to cut axb rectangle
 
+    dp[1][1] = 0;
+    for (int i = 1; i <= a; ++i)
+    {
+        for (int j = 1; j <= b; ++j)
+        {
+            if (i == j)
+            {
+                dp[i][j] = 0;
+                continue;
+            }
+            for (int k = 1; k < i; ++k)
+                dp[i][j] = min(dp[i - k][j] + dp[k][j] + 1, dp[i][j]);
+            for (int k = 1; k < j; ++k)
+                dp[i][j] = min(dp[i][j - k] + dp[i][k] + 1, dp[i][j]);
         }
     }
+
+    // for (int i = 1; i <= a; ++i)
+    // {
+    //     for (int j = 1; j <= b; ++j)
+    //         cout << dp[i][j] << " ";
+    //     cout << endl;
+    // }
+    cout << dp[a][b] << endl;
 }
