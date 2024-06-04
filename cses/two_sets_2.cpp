abb5b809adb08859e8024215ae9676d5bc5a81ea
Claire <clarrechen@gmail.com>
2024-06-04 02:17:48 -0500

    boooo

--- cses/two_sets_2.cpp
+++ cses/two_sets_2.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
 #include <vector>
-
+#define ll long long
 const int MOD = 1e9 + 7;
 
 using namespace std;
 
 /*
-dp[i][j] = number of ways to get sum j using first i numbers
-dp[i][j] = dp[i - 1][j] + dp[i - 1][j - i]
+    Given a number n, we need to find the number of ways to divide the numbers 1 to n into two sets such that the sum of the numbers in both sets is equal.
+    We can solve this problem using dynamic programming. Let dp[i][j] be the number of ways to divide the numbers 1 to i into two sets such that the sum of the numbers in both sets is j.
+    We can calculate dp[i][j] using the following recurrence:
+    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - i]
+    The answer will be dp[n][sum / 2] where sum = n * (n + 1) / 2.
+    Time complexity: O(n * sum)
 */
 int main()
 {
-    // freopen("../input.txt", "r", stdin);
-    // freopen("../output.txt", "w", stdout);
+    // dp[i][j] = dp[i-1][j] + dp[i-1][j-i]
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
     int n;
     cin >> n;
-    int sum = n / 2 * (n + 1);
+    ll sum = (n * (n + 1)) / 2;
 
     if (sum % 2 != 0)
         cout << 0 << endl;
     else
     {
         sum /= 2;
-        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+        vector<vector<ll>> dp(n + 1, vector<ll>(sum + 1, 0));
         dp[0][0] = 1;
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i <= n; ++i)
         {
-            for (int j = 0; j <= sum; j++)
+            for (int j = 0; j <= sum; ++j)
             {
                 dp[i][j] = dp[i - 1][j];
-                if (j >= i)
+                if (j - i >= 0)
                     dp[i][j] = (dp[i][j] + dp[i - 1][j - i]) % MOD;
             }
         }
-
-        cout << dp[n][sum] << endl;
+        cout << dp[n - 1][sum] << endl;
+        cout << dp[n][sum] / 2 << endl;
     }
 }
