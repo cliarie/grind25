aabb6483f761a744e5ad8f869501db3cb125fec9
Claire <clarrechen@gmail.com>
2024-06-08 23:57:25 -0500

    nlogn

--- cses/increasing_subsequence_nlogn.cpp
+++ cses/increasing_subsequence_nlogn.cpp
@@ -4,27 +4,29 @@ using namespace std;
 
 /*
 method using binary search
-dp[i] = smallest ending element of increasing subsequence of length i
-dp[i] = arr[j] where j is the largest index such that dp[j] < arr[i]
+longest_subsequece[i] = smallest element that can be the last element of an increasing subsequence of length i + 1
 */
 int main()
 {
-    // freopen("../input.txt", "r", stdin);
-    // freopen("../output.txt", "w", stdout);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
     int n;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; ++i)
         cin >> arr[i];
-    vector<int> dp;
+    vector<int> longest_subsequence;
 
     for (int i = 0; i < n; ++i)
     {
-        auto it = lower_bound(dp.begin(), dp.end(), arr[i]); // uses binary search to find the first element >= arr[i]
-        if (it == dp.end())
-            dp.push_back(arr[i]);
+        // it returns position of element in longest_subsequence >= arr[i]
+        auto it = lower_bound(longest_subsequence.begin(), longest_subsequence.end(), arr[i]); // uses binary search to find the first (smallest) element in ans array >= arr[i]
+        if (it == longest_subsequence.end())
+            longest_subsequence.push_back(arr[i]); // arr[i] > all elements in sequence, append to sequence, we have a new longest subsequence
         else
-            *it = arr[i];
+            *it = arr[i]; // replace first greater/equal to element in ans with arr[i]
     }
-    cout << dp.size() << endl;
+    for (int i = 0; i < longest_subsequence.size(); ++i)
+        cout << longest_subsequence[i] << " ";
+    cout << longest_subsequence.size() << endl;
 }
