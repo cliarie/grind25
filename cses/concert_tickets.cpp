cfec762d496059216da32a828eb491694fcfadeb
Claire <clarrechen@gmail.com>
2024-06-13 23:34:09 -0500

    concert

---
+++ cses/concert_tickets.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int n, m;
+    cin >> n >> m;
+    multiset<int> tickets;
+    for (int i = 0; i < n; ++i)
+    {
+        int x;
+        cin >> x;
+        tickets.insert(x);
+    }
+
+    for (int i = 0; i < m; ++i)
+    {
+        int x;
+        cin >> x;
+        auto it = tickets.upper_bound(x);
+        if (it == tickets.begin())
+            cout << -1 << endl;
+        else
+        {
+            it--;
+            cout << *it << endl;
+            tickets.erase(it);
+        }
+    }
+}
