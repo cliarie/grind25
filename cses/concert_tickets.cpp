6b3bda82b7f47166d2e6143adf71f33957bf041b
Claire <clarrechen@gmail.com>
2024-06-16 21:41:07 -0500

    concert tickets

--- cses/concert_tickets.cpp
+++ cses/concert_tickets.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int main()
 {
-    freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
+    // freopen("../input.txt", "r", stdin);
+    // freopen("../output.txt", "w", stdout);
 
     int n, m;
     cin >> n >> m;
-    multiset<int> tickets;
-    for (int i = 0; i < n; ++i)
+
+    int x;
+    multiset<int> tickets; // set that can contain nultiple elements with the same value
+    for (int i = 0; i < n; i++)
     {
-        int x;
         cin >> x;
         tickets.insert(x);
     }
 
-    for (int i = 0; i < m; ++i)
+    for (int i = 0; i < m; i++)
     {
-        int x;
         cin >> x;
         auto it = tickets.upper_bound(x);
         if (it == tickets.begin())
