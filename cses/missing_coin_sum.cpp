c771810f45cbce4c7f81855143aa9e3daa760bc6
Claire <clarrechen@gmail.com>
2024-06-22 18:46:40 -0500

    missing

--- cses/missing_coin_sum.cpp
+++ cses/missing_coin_sum.cpp
@@ -3,6 +3,10 @@
 #define ll long long
 using namespace std;
 
+/*
+add arr[i] to all subsets which give sum 1...currSum (min) to get subsets giving sum 1...currSum+a[i]
+break if arr[i] > currSum + 1
+*/
 int main(){
     // freopen("../input.txt", "r", stdin);
     // freopen("../output.txt", "w", stdout);
@@ -13,10 +17,10 @@ int main(){
     for (int i = 0; i < n; ++i) cin >> arr[i];
     sort(arr.begin(), arr.end());
 
-    ll min = 1;
+    ll min = 0;
     for (int i = 0; i < n; ++i){
-        if (arr[i] >= min + 1) break;
+        if (arr[i] > min + 1) break;
         else min += arr[i];
     }
-    cout << min << endl;
+    cout << min + 1 << endl;
 }
