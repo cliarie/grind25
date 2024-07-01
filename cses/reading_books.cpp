57ab3f619ed0fe31cb456b3acebf1161e96dad90
Claire <clarrechen@gmail.com>
2024-07-01 15:25:11 -0500

    reading

--- cses/reading_books.cpp
+++ cses/reading_books.cpp
@@ -3,6 +3,12 @@
 #define ll long long
 using namespace std;
 
+/*
+max delay between two readers is the max duration of a book and min duration of a book
+2 cases:
+1. after reading longest book, other reader is already done with the rest of books, then duration = 2*max
+2. after reading longest book, other reader is not done with rest of books, then duration = total duration of books
+*/
 int main(){
     // freopen("../input.txt", "r", stdin);
     // freopen("../output.txt", "w", stdout);
