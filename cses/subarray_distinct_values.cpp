a652e9d0638ac9feb595c8b301485b7f122a004c
Claire <clarrechen@gmail.com>
2024-07-06 16:17:12 -0500

    distinct

--- cses/subarray_distinct_values.cpp
+++ cses/subarray_distinct_values.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#define ll long long
 using namespace std;
 
 int main(){
-    freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
-    int n, k; cin >> n >> k;
+    // freopen("../input.txt", "r", stdin);
+    // freopen("../output.txt", "w", stdout);
+    ll n, k; cin >> n >> k;
     vector<int> arr(n);
     for (int i = 0; i < n; ++i) cin >> arr[i];
 
+    ll l = 0, r = -1, ans = 0;
+    map<ll, ll> freq;
+    while(r < n - 1){
+        r++;
+        freq[arr[r]]++;
+
+        while (freq.size() > k) {
+            if (freq[arr[l]] == 1) {
+                freq.erase(arr[l]);
+            }
+            else freq[arr[l]]--;
+            l++;
+        }
+        ans += r - l;
+
+    }
+    l++;
+    if (k >= 0) ans += n;
+    cout << ans << endl;
 }
