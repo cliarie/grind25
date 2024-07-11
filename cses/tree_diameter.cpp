997d249686ec2930ba03462a062f24254741408f
Claire <clarrechen@gmail.com>
2024-07-11 16:49:43 -0500

    diameter

--- cses/tree_diameter.cpp
+++ cses/tree_diameter.cpp
@@ -3,22 +3,20 @@
 #define pair pair<int, int>
 using namespace std;
 
-void getheight(vector<vector<int>> &adj, vector<int> &dp, int node, int parent){
+void dfs(vector<vector<int>> &adj, vector<int> &dp, int node, int parent){
     for (auto i : adj[node]){
         if (i != parent){
-            getheight(adj, dp, i, node);
-            dp[node] = max(dp[node], 1 + dp[i]);
+            dp[i] = 1 + dp[node];
+            dfs(adj, dp, i, node);
         }
     }
 }
 
 int main(){
-
 #ifdef LOCAL_TESTING
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 #endif
-
     int n; cin >> n;
     vector<vector<int>> adj(n + 1);
     for (int i = 0; i < n - 1; ++i){
@@ -27,22 +25,21 @@ int main(){
         adj[b].push_back(a);
     }
 
-    int l = 0;
+    vector<int> dp(n + 1, 0);
+    dfs(adj, dp, 1, -1);
+
+    pair farthest = {1, 0};
     for (int i = 1; i <= n; ++i){
-        vector<int> dp(n + 1, 0);
-        if (adj[i].size() == 1){
-            getheight(adj, dp, i, -1);
-            l = max(dp[i], l);
-        }
+        if (dp[i] > farthest.second) farthest = {i, dp[i]};
     }
-    cout << l << endl;
-    // for (int i = 1; i <= n; ++i) cout << i << " " << dp[i] << endl;
 
-    // int max1 = 0, max2 = 0;
-    // for (auto i : adj[1]){
-    //     if (dp[i] > max1) max1 = dp[i];
-    //     else if (dp[i] > max2) max2 = dp[i];
-    // }
+    vector<int> dp2(n + 1, 0);
+    dfs(adj, dp2, farthest.first, -1);
 
-    // cout << 2 + max1 + max2 << endl;
+    farthest.second = 0;
+    for (int i = 1; i <= n; ++i){
+        if (dp2[i] > farthest.second) farthest = {i, dp2[i]};
+    }
+
+    cout << farthest.second << endl;
 }
