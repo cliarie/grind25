79c632e6b9a9fd7b2a6d2deca182c6a7697806e4
Claire <clarrechen@gmail.com>
2024-07-11 01:40:42 -0500

    tree

--- cses/tree_matching.cpp
+++ cses/tree_matching.cpp
@@ -8,24 +8,24 @@ void dfs(vector<vector<int>>& adj, vector<vector<int>>& dp, int node, int parent
         if (i != parent){
             dfs(adj, dp, i, node);
             dp[node][0] += max(dp[i][1], dp[i][0]);
-            cout << "don't choose " << node << " " << dp[node][0] << endl;
+            // cout << "don't choose " << node << " " << dp[node][0] << endl;
         }
     }
 
     for (auto i : adj[node]){
         if (i != parent){
             dp[node][1] = max(dp[node][1], 1 + dp[i][0] + dp[node][0] - max(dp[i][0], dp[i][1]));
-            cout << "choose " << node << " " << dp[node][1] << endl;
+            // cout << "choose " << node << " " << dp[node][1] << endl;
         }
     }
 }
 
 int main(){
-    freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
+    // freopen("../input.txt", "r", stdin);
+    // freopen("../output.txt", "w", stdout);
     int n; cin >> n;
     vector<vector<int>> adj(n + 1);
-    for (int i = 0; i < n; ++i){
+    for (int i = 0; i < n - 1; ++i){
         int a, b; cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
@@ -36,7 +36,7 @@ int main(){
 
     dfs(adj, dp, 1, 0);
 
-    for (int i = 1; i <= n; ++i) cout << dp[i][0] << " " << dp[i][1] << endl;
+    // for (int i = 1; i <= n; ++i) cout << dp[i][0] << " " << dp[i][1] << endl;
 
     cout << max(dp[1][0], dp[1][1]) << endl;
 }
