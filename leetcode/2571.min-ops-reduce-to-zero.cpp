b68992ccf597ab8e54abb65f255b364a804900cc
Claire <clarrechen@gmail.com>
2024-07-17 23:07:58 -0700

    lc

---
+++ leetcode/2571.min-ops-reduce-to-zero.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+class Solution
+{
+public:
+    int minOperations(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int i = 1;
+        while (i * 2 < n)
+            i *= 2;
+        int ldiff = n - i;
+        int udiff = i * 2 - n;
+        return min(minOperations(ldiff) + 1, minOperations(udiff) + 1);
+    }
+};
