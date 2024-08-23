9a120782fc6634737513a295c87010f3f4d02e27
Claire <clarrechen@gmail.com>
2024-08-23 12:35:25 -0700

    lc

---
+++ leetcode/2918.min-eq-sum-two-arr-replace-zeros-easy-greedy.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <numeric>
+using namespace std;
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        long long s1 = accumulate(nums1.begin(), nums1.end(), 0LL), s2 = accumulate(nums2.begin(), nums2.end(), 0LL);
+        long long z1 = count(nums1.begin(), nums1.end(), 0LL), z2 = count(nums2.begin(), nums2.end(), 0LL);
+
+        if (!z1 && (s2 + z2 > s1) || !z2 && (s1 + z1 > s2)) return -1;
+
+        return max((long long)z1 + s1, (long long)z2 + s2);
+
+    }
+};
