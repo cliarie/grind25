4cfa900807f7171da18ac101e0e3aac598827795
Claire <clarrechen@gmail.com>
2024-08-23 13:33:14 -0700

    lc

--- leetcode/2918.min-eq-sum-two-arr-replace-zeros-easy-greedy.cpp
+++ leetcode/2918.min-eq-sum-two-arr-replace-zeros-easy-greedy.cpp
@@ -7,9 +7,9 @@ public:
         long long s1 = accumulate(nums1.begin(), nums1.end(), 0LL), s2 = accumulate(nums2.begin(), nums2.end(), 0LL);
         long long z1 = count(nums1.begin(), nums1.end(), 0LL), z2 = count(nums2.begin(), nums2.end(), 0LL);
 
-        if (!z1 && (s2 + z2 > s1) || !z2 && (s1 + z1 > s2)) return -1;
+        if (!z1 && ((s1 + z1) < (s2 + z2)) || !z2 && ((s2 + z2) < (s1 + z1))) return -1;
 
-        return max((long long)z1 + s1, (long long)z2 + s2);
+        return max(s1 + z1, s2 + z2);
 
     }
 };
