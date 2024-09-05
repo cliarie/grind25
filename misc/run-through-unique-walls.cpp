6f82563316b62a7a9c26ff67188ab8d19e688674
Claire <clarrechen@gmail.com>
2024-09-05 09:56:06 -0500

    problems

---
+++ misc/run-through-unique-walls.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int numDistinctWalls(const vector<double> &coords, const vector<int> &thicknesses, int energy){
+  int n = coords.size();
+  for (
+
+}
+
+
+int main(){
+  int energy;
+  vector<double> coords;
+  vector<int> thicknesses;
+  cout << numDistinctWalls(coords, thicnkesses, energy) << endl;
+}
