0e7a7ff3385baa6f8574fafa0715a52a326e37d0
Claire <clarrechen@gmail.com>
2024-09-15 04:00:24 -0500

    password reconstruction

--- 491/password-reconstruction.cpp
+++ 491/password-reconstruction.cpp
@@ -10,10 +10,6 @@ int find(ll v, vector<ll> &dsu){
     return dsu[v];
 }
 
-void join(ll a, ll b, vector<ll> &dsu){
-    dsu[a] = b;
-}
-
 int main(){
     #ifdef LOCAL_TESTING
        freopen("../input.txt", "r", stdin);
@@ -21,18 +17,47 @@ int main(){
     #endif
 
     int n; cin >> n;
-    vector<int> dsu(n + 1);
+    ll max_len = 0;
+    vector<pair<string, vector<ll>>> substrings(n);
+    for (ll i = 0; i < n; ++i){
+        string s; cin >> s;
+        ll k; cin >> k;
 
-    for(int i = 1; i <= n; i++){
+        vector<ll> indexes(k);
+        for (ll j = 0; j < k; ++j){
+            cin >> indexes[j]; indexes[j]--;
+            max_len = max(max_len, indexes[j] + (ll)s.size());
+        }
+        substrings[i] = {s, indexes};
+    }
+
+    string password(max_len, '?');
+    vector<ll> dsu(max_len + 1);
+
+    for (ll i = 0; i <= max_len; ++i){
         dsu[i] = i;
     }
 
-    for (int i = 0; i < n; ++i){
-        string s; cin >> s;
-        int k; cin >> k;
-        for (int j = 0; j < k; ++j){
-            int x; cin >> x;
-            join(x, i + 1, dsu);
+    for (auto &s : substrings){
+        string &str = s.first;
+        vector<ll> &indexes = s.second;
+        for (auto start : indexes){
+            ll idx = start;
+            int k = 0;
+            while (k < str.length()){
+                ll a = find(idx, dsu);
+                k += a - idx;
+                if (k >= str.length()) break;
+                password[a] = str[k];
+                dsu[a] = find(a + 1, dsu);
+                idx = a + 1;
+                k++;
+            }
         }
     }
+
+    for (auto &c : password){
+        if (c == '?') c = 'a';
+    }
+    cout << password << endl;
 }
