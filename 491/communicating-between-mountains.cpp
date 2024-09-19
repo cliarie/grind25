61b4c67504419b05d4e04d60381f743897cc7585
Claire <clarrechen@gmail.com>
2024-09-19 13:36:22 -0500

    9/19

--- 491/communicating-between-mountains.cpp
+++ 491/communicating-between-mountains.cpp
@@ -3,6 +3,30 @@
 #include <vector>
 using namespace std;
 
+void dfs(int u, vector<vector<int>>& graph, vector<int>& colors, int k){
+    for (int v : graph[u]){
+        if (colors[v] != -1) continue;
+        for (int c = 0; c < k; ++c){
+            bool found = false;
+            for (int w : graph[v]){
+                if (colors[w] == c){
+                    found = true;
+                    break;
+                }
+            }
+            if (!found){
+                colors[v] = c;
+                break;
+            }
+        }
+        if (colors[v] == -1){
+            cout << -1 << endl;
+            exit(0);
+        }
+        dfs(v, graph, colors, k);
+    }
+}
+
 int main(){
     #ifdef LOCAL_TESTING
        freopen("../input.txt", "r", stdin);
@@ -20,40 +44,40 @@ int main(){
     for (int i = 0; i < n; ++i){
         if (colors[i] != -1) continue;
         colors[i] = 0;
-        queue<int> q; q.push(i);
-        while (!q.empty()){
-            int u = q.front(); q.pop();
-            for (int v : graph[u]){
-                // color with max k colors
-                if (colors[v] == -1){
-                    colors[v] = (colors[u] + 1) % k;
-                    q.push(v);
-                } else if (colors[v] == colors[u]){
-                    cout << -1 << endl;
-                    return 0;
-                }
-                    // for (int c = 0; c < k; ++c){
-                    //     bool found = false;
-                    //     for (int w : graph[v]){
-                    //         if (colors[w] == c){
-                    //             found = true;
-                    //             break;
-                    //         }
-                    //     }
-                    //     if (!found){
-                    //         colors[v] = c;
-                    //         break;
-                    //     }
-                    // }
-                    // if (colors[v] == -1){
-                    //     cout << -1 << endl;
-                    //     return 0;
-                    // }
-                    // q.push(v);
-                // }
-            }
-        }
+        dfs(i, graph, colors, k);
     }
+
+    // for (int i = 0; i < n; ++i){
+    //     if (colors[i] != -1) continue;
+    //     colors[i] = 0;
+    //     queue<int> q; q.push(i);
+    //     while (!q.empty()){
+    //         int u = q.front(); q.pop();
+    //         for (int v : graph[u]){
+    //             if (colors[v] != -1) continue;
+    //             // color with max k colors
+    //             for (int c = 0; c < k; ++c){
+    //                 bool found = false;
+    //                 for (int w : graph[v]){
+    //                     if (colors[w] == c){
+    //                         found = true;
+    //                         break;
+    //                     }
+    //                 }
+    //                 if (!found){
+    //                     colors[v] = c;
+    //                     break;
+    //                 }
+    //             }
+    //             if (colors[v] == -1){
+    //                 cout << -1 << endl;
+    //                 return 0;
+    //             }
+    //             q.push(v);
+    //         }
+    //         }
+    //     }
+    // }
 
 
     for (int i = 0; i < n; ++i){
