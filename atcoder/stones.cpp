e969a432e218674159f3cc0b920edf0c5af85e4e
Claire <clarrechen@gmail.com>
2025-03-09 01:01:02 -0800

    scpt :>

--- atcoder/stones.cpp
+++ atcoder/stones.cpp
@@ -30,21 +30,42 @@ int main(){
   int n, k;
   cin >> n >> k;
   vector<int> a(k);
+  vector<int> dp(n + 1, 0);
   for (int i = 0; i < k; ++i){
     cin >> a[i];
+    dp[a[i]] = a[i];
   }
-  
-  int x, y; bool c = 1;
-  for (int i = k - 1; i >= 0 && n > 0; --i){
-    int d = n / a[i];
-    if (c) {
-      x += ((d + 1)/ 2) * a[i];
+  // dp[i] = maximum first player can take with pile of n stones
+  // dp[i] = a[k] + (i - a[k]) - (dp[i - a[k]] this is player 2)
+  for (int i = 0; i <= n; ++i){
+    for (int j = 0; j < k; ++j){
+      if (i - a[j] <= 0) break;
+      dp[i] = max(a[j] + (i - a[j]) - dp[i-a[j]], dp[i]);
+//      dp[i] = a[j] + (i - a[j]) - (dp[i-a[j]]);
     }
-    else x += (d / 2) * a[i];
-    if (d%2 != 0) c = !c;
-    n -= d * a[i];
   }
 
-  cout << x << endl;
 
+  // cannot take max at end
+  // int maxxn = 0;
+  // for (int i = 0; i <= n; ++i){
+  //   cout << dp[i] << ' ';
+  //   maxxn = max(maxxn, dp[i]);
+  // }
+
+  cout << dp[n] << '\n';
+  // not greedy
+  // int x, y; bool c = 1;
+  // for (int i = k - 1; i >= 0 && n > 0; --i){
+  //   int d = n / a[i];
+  //   if (c) {
+  //     x += ((d + 1)/ 2) * a[i];
+  //   }
+  //   else x += (d / 2) * a[i];
+  //   if (d%2 != 0) c = !c;
+  //   n -= d * a[i];
+  // }
+  //
+  // cout << x;
+  //
 }
